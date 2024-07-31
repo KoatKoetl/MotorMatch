a6ea5cb26c8f860fd72bf9aff2085fd414001240
KoatKoetl <stas20027@outlook.com>
2024-07-31 20:51:14 +0300

    Change Title

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { useEffect } from "react";
-import FeaturedBrand from "../components/section/FeaturedBrand";
-import FeaturedProduct from "../components/section/FeaturedProduct";
-import Hero from "../components/section/Hero";
-import Service from "../components/section/Service";
-import Testimonial from "../components/section/Testimonial";
-import Why from "../components/section/Why";
-import Customizable from "../components/section/Customizable";
+import { useEffect } from 'react';
+import Customizable from '../components/section/Customizable';
+import FeaturedBrand from '../components/section/FeaturedBrand';
+import FeaturedProduct from '../components/section/FeaturedProduct';
+import Hero from '../components/section/Hero';
+import Service from '../components/section/Service';
+import Testimonial from '../components/section/Testimonial';
+import Why from '../components/section/Why';
 
 export default function Home() {
   useEffect(() => {
