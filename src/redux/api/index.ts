517e2ae5db450f06ed165765916e758e90766a96
KoatKoetl <stas20027@outlook.com>
2024-08-03 00:57:07 +0300

    Change api link

--- src/redux/api/index.ts
+++ src/redux/api/index.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'productApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://motormatch-api.onrender.com',
+    baseUrl: 'https://motormatch-api.onrender.com/api',
     // baseUrl: 'http://localhost:3001/api',
   }),
   tagTypes: ['products'],
